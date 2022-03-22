e5c3b3949cffd954ad1943cad02048ba4a5863c3
Weaact <lucas.druckes@numericable.fr>
2022-03-22 16:05:18 +0100

    Fixed Rand Addicts header link to index.php

--- PHP/Header.php
+++ PHP/Header.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-
     <link rel="stylesheet" href="../ASSETS/fonts/icomoon/style.css">
 
     <!-- Bootstrap CSS -->
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- Style -->
     <link rel="stylesheet" href="../CSS/Header.css">
 
@@ -35,7 +35,7 @@
 
 
             <div class="site-logo">
-                <a href="index.html" class="text-black"><span class="text-primary">Rando Addicts </span></a>
+                <a href="../index.php" class="text-black"><span class="text-primary">Rando Addicts </span></a>
             </div>
 
             <div class="col-12">
@@ -59,6 +59,6 @@
 
     <div class="hero" style="background-image: url('../ASSETS/hero_1.jpg');"></div>
   
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
