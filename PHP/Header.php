7515b050cba9e20429c71b370c57548e6b9d7c58
Bruno FACHE <b.fache@ludus-academie.com>
2022-03-22 15:50:00 +0100

    Header
    Création de l'Header (Attention, inclure JS/CSS/Bootstrap)

---
+++ PHP/Header.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="../ASSETS/fonts/icomoon/style.css">
+
+    <!-- Bootstrap CSS -->
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="../CSS/Header.css">
+
+    <title>Rando Addicts</title>
+  </head>
+  <body>
+      <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+
+            <div class="site-logo">
+                <a href="index.html" class="text-black"><span class="text-primary">Rando Addicts </span></a>
+            </div>
+
+            <div class="col-12">
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  <li><a href="" class="nav-link">Randonnées Disponibles </a></li>
+                  <li><a href="" class="nav-link">Connexion</a></li>
+                  <li><a href="" class="nav-link">Contact</a></li>
+                </ul>
+              </nav>
+
+            </div>
+
+            <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+        </div>
+
+      </header>
+
+    <div class="hero" style="background-image: url('../ASSETS/hero_1.jpg');"></div>
+  
+
+  </body>
+</html>
