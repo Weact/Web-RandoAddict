3bd23bd0bfdb47484829920fd9c836f9f059c926
Weaact <lucas.druckes@numericable.fr>
2022-03-23 12:57:04 +0100

    Footer

--- PHP/Structure/Head.php
+++ PHP/Structure/Head.php
@@ -8,4 +8,7 @@
     <link rel="stylesheet" href="../CSS/bootstrap/bootstrap.css">
     <!-- This is the JavaScript that is used to make the Bootstrap CSS work. -->
     <script src="../JS/bootstrap/bootstrap.bundle.js"></script>
+    <!-- This is adding a link to a CSS file that is used to make the Bootstrap CSS Icons work. -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    
 </head>
