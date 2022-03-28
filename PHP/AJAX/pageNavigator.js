02b20c61ac21be292640c18ec360b1962fad2240
NetFoley <netfoley@gmail.com>
2022-03-28 14:55:35 +0200

    connect buttons

---
+++ PHP/AJAX/pageNavigator.js
@@ -0,0 +1,25 @@
+//Wait page finish loading
+let accueil = "Structure/ListeRandonneesAccueil.php"
+window.addEventListener("load", function () {
+  goTo(accueil)
+  goToOnClick(document.getElementsByClassName("goToMyRando"), "Structure/ListeRandonneesAdminPage.php")
+  goToOnClick(document.getElementsByClassName("goToDispRando"), "Structure/ListeRandonneesAccueil.php")
+  goToOnClick(document.getElementsByClassName("goToAdmin"), "Structure/PageAdmin.php")
+});
+
+function goToOnClick(btnArray, page) {
+  for(let btn of btnArray) {
+    btn.addEventListener("click", ()=>{
+      goTo(page)
+    });
+  }
+}
+
+function goTo(page) {
+    console.log("Go to " + page);
+    $.ajax({
+        url: page, success: function (result) {
+            $("#main").html(result);
+        }
+    });
+}
--- PHP/Accueil.php
+++ PHP/Accueil.php
@@ -23,18 +23,18 @@
     include_once('Structure/Head.php');
     /* Inclu le header online dans la page */
     include_once($header_path);
-    
+
 ?>
 
 <body class="h-100 text-center text-white bg-dark">
 
-    <main id="mainSection">
-    <?php
-        include_once('Structure/ListeRandonneesAccueil.php'); /* PAGE PHP A INCLURE DE BASE DANS ACCUEIL.PHP */
-        //include_once('PageRandonee.php'); /* PAGE RANDONEE, CETTE PAGE SERA INCLU DANS L'UTILISATEUR VOUDRA CONSULTER UNE RANDONEE */
-    ?>
+    <main id="main">
+      <?php
+          include_once('Structure/ListeRandonneesAccueil.php'); /* PAGE PHP A INCLURE DE BASE DANS ACCUEIL.PHP */
+          //include_once('PageRandonee.php'); /* PAGE RANDONEE, CETTE PAGE SERA INCLU DANS L'UTILISATEUR VOUDRA CONSULTER UNE RANDONEE */
+      ?>
     </main>
-    
+
 
     <?php
     /* Inclu le footer dans la page */
