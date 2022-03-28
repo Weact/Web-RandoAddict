0d58c82b6c147443ef4e8d9dc576763bdb515fe0
ZeGoblyn <j.ziegeltrum@ludus-academie.com>
2022-03-28 16:15:02 +0200

    Naming
    Ajout de nom approprié pour les GET

--- PHP/Accueil.php
+++ PHP/Accueil.php
@@ -29,8 +29,8 @@
 <body class="h-100 text-center text-white bg-dark">
 
     <?php
-          //include_once('Structure/PageAdmin.php');
-          include_once('Structure/ListeRandonneesAdminPage.php');
+          include_once('Structure/PageAdmin.php');
+        // include_once('Structure/ListeRandonneesAdminPage.php');
         /*include_once('Structure/ListeRandonneesAccueil.php'); *//* PAGE PHP A INCLURE DE BASE DANS ACCUEIL.PHP */
         /*include_once('PageRandonee.php'); /* PAGE RANDONEE, CETTE PAGE SERA INCLU DANS L'UTILISATEUR VOUDRA CONSULTER UNE RANDONEE */
     ?>
